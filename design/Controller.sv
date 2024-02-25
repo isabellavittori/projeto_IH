1be1045da3d23341085c912a4abd26e2b306af6a
Isabella Vittori <80728651+isabellavittori@users.noreply.github.com>
2024-02-25 15:48:59 -0300

    Update Controller.sv
    correção do jalrsel e RWsel

--- design/Controller.sv
+++ design/Controller.sv
@@ -17,32 +17,32 @@ module Controller (
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
     output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
     output logic Branch,  //0: branch is not taken; 1: branch is taken
-    output logic [1:0] RW_selec,
-    output logic jalr_selec
+    output logic [1:0] RWSel,
+    output logic jalrsel
 );
 
-  logic [6:0] R_TYPE, I_TYPE, U_TYPE, LW, SW, BR, JAL, JALR, HALT;
+  logic [6:0] R_TYPE, I_TYPE , U_TYPE, LW, SW, BR, JAL, JALR, HALT;
 
-  //opcode
-  assign R_TYPE = 7'b0110011;  //add,and,logico/aritimetico
-  assign I_TYPE = 7'b0010011; //immediates
-  assign U_TYPE = 7'b0110111; //LUI
-  assign LW = 7'b0000011;  //lw,loads
-  assign SW = 7'b0100011;  //sw,stores
-  assign BR = 7'b1100011;  //branches
-  assign JAL = 7'b1101111;
-  assign JALR = 7'b1100111;
-  assign HALT = 7'b0000001;
+  assign R_TYPE = 7'b0110011;  //logic instructions
+  assign I_TYPE = 7'b0010011;  //immediates
+  assign U_TYPE = 7'b0110111;  //lui
+  assign LW = 7'b0000011;      //loads
+  assign SW = 7'b0100011;      //stores
+  assign BR = 7'b1100011;      //branches
+  assign JAL = 7'b1101111;     //jal
+  assign JALR = 7'b1100111;    //jalr
+  assign HALT = 7'b0000001;    //halt
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == JALR);
+
+  assign ALUSrc =   (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == JALR);
   assign MemtoReg = (Opcode == LW);
   assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL || Opcode == JALR);
-  assign MemRead = (Opcode == LW);
+  assign MemRead =  (Opcode == LW);
   assign MemWrite = (Opcode == SW);
   assign ALUOp[0] = (Opcode == BR || Opcode == U_TYPE || Opcode == JAL || Opcode == HALT);
   assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL || Opcode == JALR || Opcode == HALT);
-  assign Branch = (Opcode == BR || Opcode == JAL || Opcode == JALR || Opcode == HALT);
-  assign RW_selec[0] = (Opcode == JAL || Opcode == JALR || Opcode == U_TYPE);
-  assign RW_selec[1] = (Opcode == U_TYPE);
-  assign jalr_selec = (Opcode == JALR);
+  assign Branch =   (Opcode == BR || Opcode == JAL || Opcode == JALR || Opcode == HALT);
+  assign RWSel[0] = (Opcode == JAL || Opcode == JALR || Opcode == U_TYPE);
+  assign RWSel[1] = (Opcode == U_TYPE);
+  assign jalrsel =  (Opcode == JALR);
 endmodule
