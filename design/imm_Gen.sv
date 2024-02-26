2f530e9a5f4a65435f45326e7366d66a85940d28
Isabella Vittori <80728651+isabellavittori@users.noreply.github.com>
2024-02-26 10:38:38 -0300

    Update imm_Gen.sv
    adicionei citação do livro para facilitar a vida

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -52,3 +52,16 @@ module imm_Gen ( //extendendo o sinal de 32-bit
     endcase
 
 endmodule
+
+//The immediate generation logic must choose between sign-
+//extending a 12-bit field in instruction bits 31:20 for load
+//instructions, bits 31:25 and 11:7 for store instructions, or bits 31, 7,
+//30:25, and 11:8 for the conditional branch. Since the input is all 32
+//bits of the instruction, it can use the opcode bits of the instruction
+//to select the proper field. RISC-V opcode bit 6 happens to be 0 for
+//data transfer instructions and 1 for conditional branches, and RISC-
+//V opcode bit 5 happens to be 0 for load instructions and 1 for store
+//instructions. Thus, bits 5 and 6 can control a 3:1 multiplexor inside
+//the immediate generation logic that selects the appropriate 12-bit
+//field for load, store, and conditional branch instructions.
+
